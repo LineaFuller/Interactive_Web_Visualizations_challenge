eb14c97a76a355e381b44271b35e7b61ff1fd364
LineaFuller <lineafuller2@gmail.com>
2022-02-04 16:46:33 -0800

    defined data

--- static/app.js
+++
@@ -1,7 +0,0 @@
-// Read json file with d3 library
-
-const dataPromise = d3.json("../../data/samples.json");
-console.log("Data Promise: ", dataPromise);
-
-// Fetch the JSON data and console log it
-d3.json("../../data/samples.json").then((data));
